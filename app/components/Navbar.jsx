0445f03199ced639e07aa6cd9ef003fda1b7868e
Samuel Mensah <samuelmensqh@gmail.com>
2023-08-16 15:05:58 +0000

    lesson 5

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
+// Logo image
+import Image from 'next/image'
+import Logo from './dojo-logo.png'
 
 
 export default function Navbar() {
   return (
     <nav>
-        <h2>Navigation</h2>
+        <Link href="/" className='inline-flex items-center gap-2'>
+            <Image 
+                src={Logo}
+                alt="Dojo Helpdesk logo"
+                width={70}
+                quality={100}
+                placeholder='blur'
+            />
+            <h2>Navigation</h2>
+        </Link>
+        
         <Link href="/">Dashboard</Link>
         <Link href="/tickets">Tickets</Link>
     </nav>
